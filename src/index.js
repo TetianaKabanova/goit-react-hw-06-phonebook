c7f179a8a9395dab9dad41a567f3644a4e7ea0d7
TetianaKabanova <110104725+TetianaKabanova@users.noreply.github.com>
2023-08-03 20:30:12 +0200

    added redux

--- src/index.js
+++ src/index.js
@@ -4,11 +4,21 @@ import { App } from 'components/App/App';
 import './index.css';
 import { ThemeProvider } from '@emotion/react';
 import { theme } from './components/theme';
+import { Provider } from 'react-redux';
+import { store, persistor } from 'redux/store';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+import Loader from 'components/Loader/Loader';
+import { ToastContainer } from 'react-toastify';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <Provider store={store}>
+        <PersistGate loading={<Loader></Loader>} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
     </ThemeProvider>
+    <ToastContainer />
   </React.StrictMode>
 );
---
+++ src/redux/store.js
@@ -0,0 +1,32 @@
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { phonebookReducer } from './phonebookReducer';
+
+const phonebookPersistConfig = {
+  key: 'phonebook',
+  storage,
+  whiteList: ['contacts'],
+};
+
+export const store = configureStore({
+  reducer: {
+    phonebook: persistReducer(phonebookPersistConfig, phonebookReducer),
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+export const persistor = persistStore(store);
